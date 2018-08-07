0b51160a6c097b7b45338124be0417fdd5956d0c
Ofek <ofekkazes2@gmail.com>
2018-08-07 18:09:54 +0300

    new

---
+++ core/assets/design/pik.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="pik" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="pik.png" trans="ff00ff" width="64" height="64"/>
+</tileset>
