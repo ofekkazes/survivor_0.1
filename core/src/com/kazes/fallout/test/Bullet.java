6033b9a93592e1e48296c69afd707cef2893034b
ofekkazes <ofekkazes2@gmail.com>
2018-08-26 21:16:24 +0300

    Started implementing Box2D and B2DLights

--- core/src/com/kazes/fallout/test/Bullet.java
+++ core/src/com/kazes/fallout/test/Bullet.java
@@ -3,46 +3,70 @@ package com.kazes.fallout.test;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Interpolation;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.forever;
-import static com.badlogic.gdx.scenes.scene2d.actions.Actions.moveBy;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class Bullet extends ImageEx {
+    private static ShaderProgram shader = new ShaderProgram(Gdx.files.internal("shaders/shakyCam.vs"), Gdx.files.internal("shaders/shakyCam.fs"));
+    private Vector2 velocity;
     private Vector2 direction;
     private float speed;
     private float timeToLive;
 
-    public Bullet(float xPos, float yPos, Vector2 direction) {
+    public Bullet(World world, float xPos, float yPos, Vector2 direction) {
         super(createTexture(xPos, yPos), xPos, yPos);
-        speed = 2;
+        speed = 100;
         timeToLive = 3;
         this.direction = direction;
-        Vector2 velocity = direction.scl(speed * 40);
-        this.addAction(forever(Actions.moveBy(velocity.x, velocity.y, 0.001f)));
+        velocity = direction.scl(speed);
+        //this.addAction(forever(Actions.moveBy(velocity.x, velocity.y, 0.001f)));
+        Gdx.app.log("Bullet", getWidth() + ", " + getHeight());
+        CircleShape circle = new CircleShape();
+        circle.setRadius(this.getWidth()/2);
+        circle.setPosition(new Vector2(getWidth() / 2, getHeight() / 2));
+        this.world = world;
+        body = B2DBodyBuilder.createBody(world, circle, xPos, yPos, getWidth(), getHeight());
+        body.setUserData(this);
+        body.setTransform(body.getWorldCenter(), MathUtils.degreesToRadians * direction.angle());
+        setOrigin(0, 0);
+        setRotation(MathUtils.radiansToDegrees * body.getAngle());
+
     }
+
     @Override
     public void act(float delta) {
         super.act(delta);
 
-        //Increase the direction by x speed, this is actually called velocity so let's name it correctly
-        Vector2 velocity = direction.scl(speed);
+        float velX = MathUtils.cos(body.getAngle()) * speed; // X-component.
+        float velY = MathUtils.sin(body.getAngle()) * speed; // Y-component.
 
-        //Calculate angle
-        float angle = velocity.angle();
+        body.setLinearVelocity(velX, velY);
 
-        //translate(velocity.x, velocity.y);
+
         timeToLive -= delta;
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+        if(timeToLive > 2.94) {
+            shader.begin();
+            shader.setUniformf("rand", new Vector3(MathUtils.random(1, 4), MathUtils.random(1, 4), 0));
+            shader.end();
+            batch.setShader(shader);
+        }
+        else batch.setShader(null);
     }
 
     public float getTimeToLive() { return timeToLive; }
 
     private static Texture createTexture(float xPos, float yPos) {
-        Pixmap pixmap = new Pixmap( 64, 64, Pixmap.Format.RGBA8888 );
+        Pixmap pixmap = new Pixmap( 16, 16, Pixmap.Format.RGBA8888 );
+
         pixmap.setColor( 0, 0, 0, 1 );
-        pixmap.fillCircle( 32, 32, 4 );
+        pixmap.fillCircle( 8, 8, 8 );
         Texture pixmaptex = new Texture( pixmap );
         pixmap.dispose();
         return  pixmaptex;
--- core/src/com/kazes/fallout/test/ImageEx.java
+++ core/src/com/kazes/fallout/test/ImageEx.java
@@ -7,45 +7,21 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class ImageEx extends Image {
-
-    Texture texture;
-    public boolean xFlipped;
-    boolean yFlipped;
-
-    private ImageEx flipped;
-    private ImageEx original;
-
+    private boolean remove;
     private Rectangle rectangle;
+    World world;
+    Body body;
 
-    public ImageEx(Texture texture) {
-        super(texture);
-        this.texture = texture;
-        xFlipped = false;
-        yFlipped = false;
 
-        original = new ImageEx(new TextureRegion(texture,0,0, texture.getWidth(), texture.getHeight()));
-        flipped = new ImageEx(ImageEx.flipTexture(texture, true, false));
-
-        this.rectangle = new Rectangle(this.getX(), this.getY(), this.getWidth(), this.getHeight());
-    }
     public ImageEx(Texture texture, float xPos, float yPos) {
         super(texture);
-        this.texture = texture;
-        xFlipped = false;
-        yFlipped = false;
-
-        original = new ImageEx(new TextureRegion(texture,0,0, texture.getWidth(), texture.getHeight()));
-        flipped = new ImageEx(ImageEx.flipTexture(texture, true, false));
-
         this.setPosition(xPos, yPos);
-
         this.rectangle = new Rectangle(this.getX(), this.getY(), this.getWidth(), this.getHeight());
     }
-
-    public ImageEx(TextureRegion texture) { this(texture, 0, 0); }
 
     public ImageEx(TextureRegion texture, float xPos, float yPos) {
         super(texture);
@@ -53,52 +29,42 @@ public class ImageEx extends Image {
         this.rectangle = new Rectangle(this.getX(), this.getY(), this.getWidth(), this.getHeight());
     }
 
-    public ImageEx(Texture texture, boolean xFlip, boolean yFlip) {
-        super(ImageEx.flipTexture(texture, xFlip, yFlip));
-        this.rectangle = new Rectangle(this.getX(), this.getY(), this.getWidth(), this.getHeight());
 
-    }
 
     @Override
     public void act(float delta) {
         super.act(delta);
         this.rectangle.setX(this.getX());
         this.rectangle.setY(this.getY());
+        if(body != null) {
+            if(body.isAwake()) {
+                setX(body.getPosition().x);
+                setY(body.getPosition().y);
+            }
+        }
 
+        if(remove) {
+            if(body != null)
+                world.destroyBody(body);
+            this.remove();
+        }
     }
+
 
     public void translate(float x, float y) {
         this.setPosition(this.getX() + x, this.getY() + y);
-    }
-
-    public void translateX(float x) {
-        translate(x, 0);
-    }
-
-    public void translateY(float y) {
-        translate(0, y);
-    }
-
-    public void flip(boolean xFlip, boolean yFlip) {
-        if(!xFlipped)
-            this.setDrawable(flipped.getDrawable());
-        else
-            this.setDrawable(original.getDrawable());
-        xFlipped = !xFlipped;
     }
 
     public Rectangle getRectangle() {
         return this.rectangle;
     }
 
-    public static TextureRegion flipTexture(Texture texture, boolean xFlip, boolean yFlip) {
-        TextureRegion tmp = new TextureRegion(texture,0,0, texture.getWidth(), texture.getHeight());
-        tmp.flip(xFlip, yFlip);
-        return tmp;
-    }
+    public Body getBody() { return this.body; }
 
     public Vector2 getOrigin() {
         return new Vector2(getX() + getWidth() / 2, getY() + getHeight() / 2);
     }
+
+    public void setRemove() { this.remove = true; }
 
 }
