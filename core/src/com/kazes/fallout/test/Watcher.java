a03cb4b23738dd40822eb3d1a3bff4fc164c35f5
ofekkazes <ofekkazes2@gmail.com>
2018-09-01 17:41:54 +0300

    Added physics

--- core/src/com/kazes/fallout/test/Watcher.java
+++ core/src/com/kazes/fallout/test/Watcher.java
@@ -2,8 +2,11 @@ package com.kazes.fallout.test;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.kazes.fallout.test.screens.GameScreen;
+import com.kazes.fallout.test.physics.B2DBodyBuilder;
+import com.kazes.fallout.test.physics.CollisionCategory;
 
 import static com.badlogic.gdx.scenes.scene2d.actions.Actions.delay;
 import static com.badlogic.gdx.scenes.scene2d.actions.Actions.sequence;
@@ -14,7 +17,7 @@ public class Watcher extends NPC {
     private float startX, startY, endX, endY;
     private float time;
     private Watcher follow;
-    public Watcher(float startX, float startY, float endX, float endY, float time) {
+    public Watcher(World world, float startX, float startY, float endX, float endY, float time) {
         super(Assets.getAsset(Assets.Images.PIKACHU, Texture.class), "Watcher", startX, startY, Weapons.Pistol);
         this.startX = startX;
         this.startY = startY;
@@ -22,10 +25,18 @@ public class Watcher extends NPC {
         this.endY = endY;
         this.time = time;
         this.ascending = true;
+        initPhysics(world);
     }
-    public Watcher(Watcher follow) {
+    public Watcher(World world, Watcher follow) {
         super(Assets.getAsset(Assets.Images.PIKACHU, Texture.class), "Watcher", follow.getX(), follow.getY(), Weapons.Pistol);
         this.follow = follow;
+        initPhysics(world);
+    }
+
+    public void initPhysics(World world) {
+        this.world = world;
+        this.body = B2DBodyBuilder.createBody(world, getX(), getY(), getWidth(), getHeight(), BodyDef.BodyType.DynamicBody, CollisionCategory.FRIENDLY, CollisionCategory.FRIENDLY_COLLIDER);
+        this.body.setFixedRotation(true);
     }
 
     @Override
