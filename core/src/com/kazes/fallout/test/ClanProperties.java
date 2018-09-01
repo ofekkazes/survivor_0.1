8160110760c96f00e342f3d9ca31b8e80a5fef00
ofekkazes <ofekkazes2@gmail.com>
2018-09-01 17:42:27 +0300

    Will be read from json

---
+++ core/src/com/kazes/fallout/test/ClanProperties.java
@@ -0,0 +1,9 @@
+package com.kazes.fallout.test;
+
+public class ClanProperties {
+    public static int Warriors = 0;
+    public static int Doctors = 0;
+    public static boolean Merchant = false;
+    public static int PayedMercenaries = 0;
+    public static int Scientists = 0;
+}
