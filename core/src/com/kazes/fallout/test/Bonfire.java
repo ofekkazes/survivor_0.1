a28cff80c38aba3f36e0a4027f38b8c97f6de3b5
ofekkazes <ofekkazes2@gmail.com>
2018-09-06 17:45:15 +0300

    added point light

--- core/src/com/kazes/fallout/test/Bonfire.java
+++ core/src/com/kazes/fallout/test/Bonfire.java
@@ -1,14 +1,19 @@
 package com.kazes.fallout.test;
 
 
+import box2dLight.PointLight;
+import box2dLight.RayHandler;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.utils.Timer;
+import com.kazes.fallout.test.screens.GameScreen;
 
 public class Bonfire extends ImageEx {
     float timeout;
-    public Bonfire(float xPos, float yPos) {
+    public Bonfire(float xPos, float yPos, RayHandler rayHandler) {
         super(Assets.getAsset(Assets.Images.BONFIRE, Texture.class), xPos, yPos);
         timeout = 60 * 5;
+        new PointLight(rayHandler, 200, Color.FIREBRICK, 5, getX() + getWidth() / 2, yPos + getHeight() / 2);
     }
 
     @Override
