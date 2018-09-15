6546757b9c659c492dc89184be2beb2092dc507a
ofekkazes <ofekkazes2@gmail.com>
2018-09-15 21:43:48 +0300

    Numbers to fast inventory selection

---
+++ core/src/com/kazes/fallout/test/InputHelper.java
@@ -0,0 +1,8 @@
+package com.kazes.fallout.test;
+
+import com.badlogic.gdx.Input;
+
+public class InputHelper {
+    public static int fastInventoryKeys[] = {Input.Keys.NUM_1, Input.Keys.NUM_2, Input.Keys.NUM_3,
+                                             Input.Keys.NUM_4, Input.Keys.NUM_5};
+}
